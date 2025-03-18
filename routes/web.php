53688a91a5136441eb98f489dac8352e09b3a040
gomezfarm <gomez.farm@outlook.com>
2025-03-18 10:38:45 -0600

    Parametros a las rutas y parametros opcionales

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,27 @@ Route::get('/', function () {
 Route::match(['get', 'post'], '/contacto', function () {
     return "Hola desde la pagina de contacto usando el metodo GETo POST";
 });
+
+// Tenemos las rutas que se van creando de manera dinamica que segun lo que cambia en la URL muestra uno 
+// u otro contenido (Lo que este entre {} va a depender de lo que el usuario ingrese en la URL)
+// El nombre que se le pase entre {} tiene que ser el mismo nombre del argumento de la funcion
+Route::get('/cursos/{curso}', function($curso){
+    // Lo que el usuario introdusca en {curso} lo podremos mostrar con $curso
+    return "Bienvenido al curso: $curso";
+});
+
+// Laravel compara los URL de arriba para abajo, por lo que si una remplaza a otra, sera la que este mas arriba la que se muestre
+// Asi toma la parte de "information" como si fuera la ruta dinamica (La tenemos que mover mas arriba antes de la dinamica)
+Route::get('/cursos/informacion', function () {
+    return "Aqui esta la informacion del curso";
+});
+
+// Recibir mas de un parametro en ruta dinamica
+// Para que uno de los parametros sean opcionales le colocamos el simbolo ? y en la funcion asignarle un valor por defecto
+Route::get('/cursos2/{curso}/{category?}', function($curso, $category = null){
+    if($category){
+        return "Bienvenido al curso: $curso y de la cateogoria $category";
+    }else{
+        return "Bienvenido al curso: $curso";
+    }
+});
