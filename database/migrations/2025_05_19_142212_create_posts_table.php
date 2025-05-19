13cd96d0b98293d2c7bc53173c39ea2d3c45ba67
gomezfarm <gomez.farm@outlook.com>
2025-05-19 08:57:07 -0600

    Crear Tablas con Migraciones

---
+++ database/migrations/2025_05_19_142212_create_posts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // Gracias a la convencion de ejecutar el comando como: create_NombreTabla_table, nos crea este codigo
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            // Estos son los campos que tendria esta tabla Posts
+            // Podemos limitar la cantidad de caracteres que queremos especificando el numero que queremos permitir
+            $table->string('title', 15);
+            $table->string('slug'); // El SLUG es la URL donde se muestra el titulo separado por guiones
+            $table->text('body');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
