8f92c353b3a60806c5e356ff4252e96919fbfca7
gomezfarm <gomez.farm@outlook.com>
2025-05-20 20:56:43 -0600

    Modificar columnas de la tabla

---
+++ database/migrations/2025_05_21_024749_alter_to_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            // En nuestra tabla tenemos un campo con un tipo de dato que ocupa mucho espacio y lo queremos cambiar a uno que ocupe menos tamano
+            // La creamos con el nuevo tipo de dato, mismo nombre
+            $table->mediumText('body')
+                // Tenemos que especificar todos los modificadores que queramos mantener porque si no los especificamos nos los quitara
+                ->nullable()
+                ->change(); // Asi le decimos que modifique la columna con las nuevas propiedades especificadas
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            // Ponemos la columna a sus modificadores originales
+            $table->longText('body')
+                ->change();
+        });
+    }
+};
