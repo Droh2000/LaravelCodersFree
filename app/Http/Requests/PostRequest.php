12fe422457e0945c6c9edefa782bbcb04d2974b1
gomezfarm <gomez.farm@outlook.com>
2025-06-04 15:37:15 -0600

    Regla Unique - Mostrar mensajes correctos de validacion para el Slug en Crear y Editar

--- app/Http/Requests/PostRequest.php
+++ app/Http/Requests/PostRequest.php
@@ -22,10 +22,30 @@ class PostRequest extends FormRequest
      */
     public function rules(): array
     {
+        // Como estamos usando el mismo Form Request ya sea para edicion o para recuest
+        // si nos vamos al de Crear Post la variable "$this->post" nos estara mandando NULL
+        // Asi que verificamos si tenemos algo en esa variable
+        if($this->post){
+            // De existir almacenamos el ID y le concatenamos un Coma adelante porque asi se la tenemos que enviar al metodo
+            // de validacion del slug
+            $post_id = ',' . $this->post->id;
+        }else{
+            $post_id = '';
+        }
+
         // Aqui colocamos las reglas de validacion que queremos reutilizar
         return [
             'title' => 'required',
-            'slug' => 'required',
+            // Agregamos una regla adicional, para eso pusimos la barra |
+            // Despues indicamos que ese campo debe ser unico y lo debe comparar en la tabla Post, con esto laravel verifica
+            // que esta llegando en el campo Slug sea unico en el campo Slug de la tabla Post, pero en el caso que el campo de la tabla
+            // no conicida con el valor que tenemos aqui colocamos Coma y especificamos en cual campo queremos que haga la comparacion
+            //      'slug' => 'required|unique:posts, slug',
+            // Esta es otra forma de agregar la regla de validacion
+            // Esto ya no funcionara mostrando el mensaje en el HTML pero al querer editar, nos saldra que tenemos un problema, porque si queremos editar
+            // queremos que compruebe que en efecto sea un Slug unico pero que omita la verificacion con sigo mismo (Con el Id del registro que estamos editando)
+            // Colocamos una segunda Coma y le especificamos el Id con el cual no queremos que haga la verificacion ($this->post Recuperamos el valor del parametro Post $post que nos mandan al controlador)
+            'slug' => ['required','unique:posts, slug' . $post_id],
             'body' => 'required',
             'category_id' => 'required',
         ];
