2ce06091d4c8e75c1ea97a7f957b45fe9b611cce
gomezfarm <gomez.farm@outlook.com>
2025-06-17 15:43:14 -0600

    Insertar registros de pruebas para categorias

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,10 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
+    // Para poder ejecutar los factories le agregamos esto al modelo
+    use HasFactory;
+
     // Aqui y en los demas modelos Habilitamos la asignacion masiva
     protected $fillable = ['name'];
 
