ee42b1e52760fca7d2f8d706374fd9fcfdc6f77c
gomezfarm <gomez.farm@outlook.com>
2025-06-17 16:40:16 -0600

    Listar Categorias

---
+++ resources/views/admin/categories/index.blade.php
@@ -0,0 +1,46 @@
+<!-- Ingresamos a la carpeta "layouts" y nos traiga el componente 'admin'
+        Este codifo de Flux lo sacamos de la pagina oficial
+-->
+<x-layouts.admin>
+    <flux:breadcrumbs class="mb-4">
+        <flux:breadcrumbs.item href="{{ route('admin.dashboard') }}">Dashboard</flux:breadcrumbs.item>
+        <!-- Esta es la opcion del menu en el que nos encontramos -->
+        <flux:breadcrumbs.item>
+            Categorias
+        </flux:breadcrumbs.item>
+    </flux:breadcrumbs>
+
+    <!-- Mostrar el listado de las categorias -->
+    <div class="relative overflow-x-auto">
+        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                    <th scope="col" class="px-6 py-3">
+                        Id
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Name
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Edit
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($categories as $category)
+                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            {{ $category->id }}
+                        </th>
+                        <td class="px-6 py-4">
+                            {{ $category->name }}
+                        </td>
+                        <td class="px-6 py-4">
+
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</x-layouts.admin>
--- routes/admin.php
+++ routes/admin.php
@@ -1,4 +1,6 @@
 <?php
+
+use App\Http\Controllers\Admin\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 // Aqui vamos a definir las rutas que solo los administradores podran acceder
@@ -10,3 +12,6 @@ Route::get('/', function() {
 // Tenemos que indicarle a laravel que tenemos este nuevo archivo de rutas para que lo tome en cuenta
 // agregamos la siguiente linea: "require __DIR__.'/admin.php'" en alguno de los archivos de rutas que ya bienen por defecto
 // De otra lo configuramos en "bootstrap/app.php"
+
+// Nos genere todas las rutas de categorias
+Route::resource('categories', CategoryController::class);
