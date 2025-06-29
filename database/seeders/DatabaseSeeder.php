5c0d7c2038409a16e701b8467bd6040754ae0463
gomezfarm <gomez.farm@outlook.com>
2025-06-29 11:46:31 -0600

    Hacer que se limpiern directorios al limpiar el proyecto y se vuelva a crear

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,6 +7,7 @@ use App\Models\Post;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,6 +16,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // Supongamos que limpiamos los datos de nuestro proyecto ejecutando el comando migrate --fresh, esto nos borrara todos los datos y volvera a crearlos
+        // pero resulta que las imagenes que habiamos subido se mantienen aunque ya no esten asociadas a ningun post, asi que vamos a hacer que limpie estas imagenes
+        Storage::deleteDirectory('posts'); // Esto nos borrar esta carpeta (Si no existe no hace nada)
+        Storage::makeDirectory('posts'); // Supongamos que requerimos que por el funcionamiento de nuestra web esta carpeta debe exsitir si o si
+
         // User::factory(10)->create();
 
         User::factory()->create([
