ae1a751c59de3501af414cdf873d4777148fa554
gomezfarm <gomez.farm@outlook.com>
2025-06-29 11:35:16 -0600

    Copair y mover archivos

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
 use Livewire\Volt\Volt;
 
 Route::get('/', function () {
@@ -20,6 +21,19 @@ Route::middleware(['auth'])->group(function () {
     Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
     Volt::route('settings/password', 'settings.password')->name('settings.password');
     Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
+});
+
+// Definimos esta ruta solo para ver como podemos copiar un archivo hacia otra carpeta
+Route::get('/prueba', function () {
+    $path = "RUTA/Nombre.extencion";
+    $target = "NUEVA_RUTA/Nombre.extencion"; // Aqui le podramos dar otro nombre
+
+    // Storage::copy($path, $target);
+
+    // Si queremos mover el archivo
+    Storage::move($path, $target);
+
+    return 'Imagen copiada';
 });
 
 // Aqui en el archivo de rutas podemos ver que esta llamando a este archivo (Este tambien contiene Rutas)
